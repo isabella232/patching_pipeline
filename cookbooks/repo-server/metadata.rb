4c9c92ce7d4ca39f954a855ed5ecb08b12c9c4a0
Bill Meyer <bill@chef.io>
2016-05-11 13:29:54 -0500

    New cookbook for provisioning the repo-server

---
+++ cookbooks/repo-server/metadata.rb
@@ -0,0 +1,7 @@
+name 'repo-server'
+maintainer 'Bill Meyer'
+maintainer_email 'bill@chef.io'
+license 'all_rights'
+description 'Installs/Configures repo-server'
+long_description 'Installs/Configures repo-server'
+version '0.1.5'
